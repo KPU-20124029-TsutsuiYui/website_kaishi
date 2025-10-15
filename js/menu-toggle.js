aee00dfb0f0f0c8c68d1f0f734dd3e53965b1d08
KPU-20124029-TsutsuiYui <20124029@kaishi-pu.ac.jp>
2025-10-15 13:32:48 +0900

    Add files via upload

---
+++ js/menu-toggle.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("navMenu");
+    
+    hamburger.addEventListener("click", function(){
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+    
+    document.querySelectorAll("nav a").forEach(link => {
+        link.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        });
+    });
+});
