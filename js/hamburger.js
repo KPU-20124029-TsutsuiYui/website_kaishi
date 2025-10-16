e7adbd30fd34b71a8a242e3f700906aad9d0ff11
KPU-20124029-TsutsuiYui <20124029@kaishi-pu.ac.jp>
2025-10-16 13:35:48 +0900

    Update hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,19 +1,19 @@
-// 開閉制御
-document.addEventListener("DOMContentLoaded", function (){
+document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.getElementById("hamburger");
-    const navMenu = document.getElementById("navMenu");
-
-  // メニュー表示/非表示
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  });
-
-  // メニューを閉じる
-  navMenu.querySelectorAll("a").forEach(link => {
-    link.addEventListener("click", () => {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
+    const nav = document.getElementById("nav");
+    const navLinks = nav.querySelectorAll("a");
+    
+    // クリックで開閉
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        nav.classList.toggle("active");
     });
-  });
+    
+    // メニュー項目をクリックしたら閉じる
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            nav.classList.remove("active");
+        });
+    });
 });
