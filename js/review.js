d2306b2565140773790fae04cf0e6438150522bc
KPU-20124029-TsutsuiYui <20124029@kaishi-pu.ac.jp>
2025-10-23 14:33:43 +0900

    Update review.js

--- js/review.js
+++ js/review.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (menuFilter) togglePlaceholderStyle(menuFilter);
 
     // data.JSONからメニューデータを非同期で取得
-    fetch("https://raw.githubusercontent.com/KPU-20124029-TsutsuiYui/website_kaishi/refs/heads/main/JSON/data.JSON")
+    fetch('https://raw.githubusercontent.com/KPU-20124029-TsutsuiYui/website_kaishi/refs/heads/main/JSON/data.JSON')
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
@@ -295,4 +295,5 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
 
